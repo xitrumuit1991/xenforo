65e3ecfc3a281c37a8797e0bef6c8a046ce5a460
knguyen <nguyentvk@hdviet.com>
2016-07-15 11:19:47 +0700

    init user admin nguyen

--- library/config.php
+++ library/config.php
@@ -1,10 +1,31 @@
 <?php
+/*default localhost*/
+// $config['db']['host'] = 'localhost';
+// $config['db']['port'] = '3306';
+// $config['db']['username'] = 'root';
+// $config['db']['password'] = '';
+// $config['db']['dbname'] = 'xenforo';
 
-$config['db']['host'] = 'localhost';
-$config['db']['port'] = '3306';
-$config['db']['username'] = 'root';
-$config['db']['password'] = '';
-$config['db']['dbname'] = 'xenforo';
+
+
+/*custom host mysql*/
+// host : "dev3.fimplus.io"  
+//port : 23306  
+//user : "hd1dev"  
+//password : "fim+dev"
+$config['db']['host'] = 'dev3.fimplus.io';
+$config['db']['port'] = '23306';
+$config['db']['username'] = 'hd1dev';
+$config['db']['password'] = 'fim+dev';
+$config['db']['dbname'] = 'information_xfr';
+
+
+/*custom host mysql*/
+//host : badger.vodien.com 
+//port 3306
+//user: topnotch_nail
+//pass: xitrumuit1991@
+
 
 
 $config['superAdmins'] = '1';
